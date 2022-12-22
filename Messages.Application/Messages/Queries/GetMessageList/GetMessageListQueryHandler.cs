285e858a66ea35c167c3cd94b768ff32e07dedd9
MrPrograMax <grigorievma9461@gmail.com>
2022-12-22 23:34:44 +0700

    fix

--- Messages.Application/Messages/Queries/GetMessageList/GetMessageListQueryHandler.cs
+++ Messages.Application/Messages/Queries/GetMessageList/GetMessageListQueryHandler.cs
@@ -19,6 +19,7 @@ namespace Messages.Application.Messages.Queries.GetMessageList
         public async Task<MessageListVm> Handle(GetMessageListQuery request, CancellationToken cancellationToken)
         {
             var messagesQuery = await _dbContext.Messages
+                .Where(m => m.UserId == request.UserId)
                 .ProjectTo<MessageLookupDto>(_mapper.ConfigurationProvider)
                 .ToListAsync(cancellationToken);
 
